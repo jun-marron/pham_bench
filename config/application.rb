405efff0158e48f8e65d4c39a696510dfcd85dca
jun-marron <j.kimura42016891@gmail.com>
2018-03-31 09:27:49 +0900

    installed bootstrap

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,6 @@ module PhamBench
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
   end
 end
