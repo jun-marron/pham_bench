a381db0729a641c8cf078b824184fd6cd6603db7
jun-marron <j.kimura42016891@gmail.com>
2018-05-01 00:59:08 +0900

    signup signin 作成1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+     :registrations => "registrations"
+   }
   resources :users, only: [:show]
   root  'static_pages#home'
-
+  get 'users/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
