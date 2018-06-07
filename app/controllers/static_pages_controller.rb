f53ce4104a7477b9160a03b62928ae5dbbae42ae
jun-marron <j.kimura42016891@gmail.com>
2018-06-08 02:43:26 +0900

    nomethod error microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,6 @@ class StaticPagesController < ApplicationController
   def home
     if signed_in?
       @micropost = current_user.microposts.build
-      @feed = current_user.feed.paginate(page: params[:page])
     end
   end
 end
