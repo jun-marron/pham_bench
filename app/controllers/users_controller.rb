ed2024556704d371eeab46d2ddef8210234c5d3b
fumimi <info@fumimi.jp>
2018-06-08 21:45:55 +0900

    ユーザー詳細画面にユーザーを表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,26 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    begin
+      @user = User.find(params[:id])
+    rescue => e
+      flash[:notice] = "エラーが発生しました。"
+      redirect_to root_path
+    end
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
       redirect_to @user
     else
       render 'new'
+    end
   end
-end
 
-private
+  private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
