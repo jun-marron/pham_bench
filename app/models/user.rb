d953fd2b578ad919a90c1415d8a6051ddd06f19f
jun-marron <j.kimura42016891@gmail.com>
2018-06-11 21:23:13 +0900

    user_model def feed削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  def feed
-    Micropost.where("user_id = ?", id)
-  end
 end
